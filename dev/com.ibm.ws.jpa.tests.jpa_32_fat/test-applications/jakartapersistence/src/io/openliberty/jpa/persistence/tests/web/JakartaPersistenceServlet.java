da5b10a99e5872aaa984fb57805a480be58bcf8d
Aiswarya R <aiswarya.r6@ibm.com>
2025-06-06 09:52:04 +0530

    Added FAT to test extract() with YEAR and QUARTER field from java.time.LocalDate

--- dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartapersistence/src/io/openliberty/jpa/persistence/tests/web/JakartaPersistenceServlet.java
+++ dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartapersistence/src/io/openliberty/jpa/persistence/tests/web/JakartaPersistenceServlet.java
@@ -12,11 +12,10 @@ package io.openliberty.jpa.persistence.tests.web;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
-import io.openliberty.jpa.persistence.tests.models.User;
-
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.junit.Test;
@@ -24,8 +23,10 @@ import org.junit.Test;
 import componenttest.app.FATServlet;
 import io.openliberty.jpa.persistence.tests.models.Priority;
 import io.openliberty.jpa.persistence.tests.models.Product;
+import io.openliberty.jpa.persistence.tests.models.QueryDateTimeEntity;
 import io.openliberty.jpa.persistence.tests.models.Ticket;
 import io.openliberty.jpa.persistence.tests.models.TicketStatus;
+import io.openliberty.jpa.persistence.tests.models.User;
 import jakarta.annotation.Resource;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -72,8 +73,9 @@ public class JakartaPersistenceServlet extends FATServlet {
         assertNotNull(exceptResult);
     }
 
-     /**
+    /**
      * Method for testing || in JPQL queries.
+     *
      * @throws Exception
      */
     @Test
@@ -83,29 +85,32 @@ public class JakartaPersistenceServlet extends FATServlet {
         User user1 = User.of(1, "John", "Doe");
         User user2 = User.of(2, "Harry", "Potter");
         User user3 = User.of(3, "Hermione", "Granger");
-        
+
         tx.begin();
         em.persist(user1);
         em.persist(user2);
         em.persist(user3);
         tx.commit();
 
-        try{
-            String concatJPQL = "SELECT u.firstName || ' ' || u.lastName FROM User u where u.lastName = ?1" ;
-	        String fullName = em.createQuery(concatJPQL,String.class).setParameter(1, "Doe")
-	            					.getSingleResult();
+        try {
+            String concatJPQL = "SELECT u.firstName || ' ' || u.lastName FROM User u where u.lastName = ?1";
+            String fullName = em.createQuery(concatJPQL, String.class)
+                            .setParameter(1, "Doe")
+                            .getSingleResult();
 
-            String concatJPQLFrom = "SELECT u.firstName FROM User u where u.firstName || ' ' || u.lastName = ?1" ;
-	        String firstName= em.createQuery(concatJPQLFrom,String.class).setParameter(1, "Harry Potter")
-	            					.getSingleResult();
+            String concatJPQLFrom = "SELECT u.firstName FROM User u where u.firstName || ' ' || u.lastName = ?1";
+            String firstName = em.createQuery(concatJPQLFrom, String.class)
+                            .setParameter(1, "Harry Potter")
+                            .getSingleResult();
 
             assertEquals("John Doe", fullName);
             assertEquals("Harry", firstName);
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             throw e;
         }
     }
+
     /**
      * In previous version, Enumerated annotations were used for mapping Java Enum types to database column values.
      *
@@ -134,7 +139,7 @@ public class JakartaPersistenceServlet extends FATServlet {
         em.persist(ticket3);
         tx.commit();
 
-         /*
+        /*
          * The INSERT statements present in the log is missing value mapping:
          * INSERT INTO TICKET (ID, NAME, PRIORITY, STATUS) VALUES (?, ?, ?, ?)
          * bind => [1, ticket1, HIGH, 0]
@@ -173,7 +178,7 @@ public class JakartaPersistenceServlet extends FATServlet {
 
     }
 
-     /**
+    /**
      * Specifies the precedence of null values within query result sets.
      * https://jakarta.ee/specifications/persistence/3.2/jakarta-persistence-spec-3.2#a5587
      *
@@ -287,6 +292,76 @@ public class JakartaPersistenceServlet extends FATServlet {
         }
         assertEquals(3, productsNullLast.size());
         assertEquals("Sorted based on 'description' in desc order with NULLS LAST, Expecting last element to be 'product2'", "product2", productsNullLast.get(2).name);
+    }
+
+    /**
+     * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
+     * this test extract the calendar year from java.time.LocalDate
+     */
+    @Test
+    public void testExtractYearFromLocalData() throws Exception {
+        deleteAllEntities(QueryDateTimeEntity.class);
+        QueryDateTimeEntity q1 = new QueryDateTimeEntity(1, "q1", LocalDate.of(2022, 06, 07), LocalTime.of(12, 0), LocalDateTime.of(2022, 06, 07, 12, 0));
+        QueryDateTimeEntity q2 = new QueryDateTimeEntity(2, "q2", LocalDate.of(2020, 12, 31), LocalTime.of(01, 59), LocalDateTime.of(2020, 12, 31, 01, 59));
+        QueryDateTimeEntity q3 = new QueryDateTimeEntity(3, "q3", LocalDate.of(2021, 01, 01), LocalTime.of(00, 30), LocalDateTime.of(2021, 01, 01, 00, 30));
+        QueryDateTimeEntity q4 = new QueryDateTimeEntity(10000);
+
+        tx.begin();
+        em.persist(q1);
+        em.persist(q2);
+        em.persist(q3);
+        em.persist(q4);
+        tx.commit();
+
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Integer> criteriaQuery = criteriaBuilder.createQuery(Integer.class);
+        Root<QueryDateTimeEntity> from = criteriaQuery.from(QueryDateTimeEntity.class);
+        jakarta.persistence.criteria.LocalDateField<Integer> YEAR = jakarta.persistence.criteria.LocalDateField.YEAR;
+        jakarta.persistence.criteria.Expression<Integer> year = criteriaBuilder.extract(YEAR, from.get("localDateData"));
+        criteriaQuery.select(year);
+        criteriaQuery.orderBy(criteriaBuilder.desc(from.get("name"), Nulls.FIRST));
+        List<Integer> result = em.createQuery(criteriaQuery).getResultList();
+        assertEquals(4, result.size());
+        assertEquals(null, result.get(0));
+        assertEquals("Extracted Year should be 2021", Integer.valueOf(2021), result.get(1));
+        assertEquals("Extracted Year should be 2020", Integer.valueOf(2020), result.get(2));
+        assertEquals("Extracted Year should be 2022", Integer.valueOf(2022), result.get(3));
+
+    }
+
+    /**
+     * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
+     * this test extract the QUARTER of the year numbered from 1 to 4 from java.time.LocalDate
+     */
+    @Test
+    public void testExtractQuarterFromLocalData() throws Exception {
+        deleteAllEntities(QueryDateTimeEntity.class);
+        QueryDateTimeEntity q1 = new QueryDateTimeEntity(1, "q1", LocalDate.of(2022, 06, 07), LocalTime.of(12, 0), LocalDateTime.of(2022, 06, 07, 12, 0));
+        QueryDateTimeEntity q2 = new QueryDateTimeEntity(2, "q2", LocalDate.of(2020, 12, 31), LocalTime.of(01, 59), LocalDateTime.of(2020, 12, 31, 01, 59));
+        QueryDateTimeEntity q3 = new QueryDateTimeEntity(3, "q3", LocalDate.of(2021, 01, 01), LocalTime.of(00, 30), LocalDateTime.of(2021, 01, 01, 00, 30));
+        QueryDateTimeEntity q4 = new QueryDateTimeEntity(10000);
+
+        tx.begin();
+        em.persist(q1);
+        em.persist(q2);
+        em.persist(q3);
+        em.persist(q4);
+        tx.commit();
+
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Integer> criteriaQuery = criteriaBuilder.createQuery(Integer.class);
+        Root<QueryDateTimeEntity> from = criteriaQuery.from(QueryDateTimeEntity.class);
+        jakarta.persistence.criteria.LocalDateField<Integer> QUARTER = jakarta.persistence.criteria.LocalDateField.QUARTER;
+        jakarta.persistence.criteria.Expression<Integer> quarter = criteriaBuilder.extract(QUARTER, from.get("localDateData"));
+        criteriaQuery.select(quarter);
+        criteriaQuery.orderBy(criteriaBuilder.desc(from.get("name"), Nulls.FIRST));
+        List<Integer> result = em.createQuery(criteriaQuery).getResultList();
+        assertEquals(4, result.size());
+        assertEquals(null, result.get(0));
+        assertEquals("Extracted Year should be 2021", Integer.valueOf(1), result.get(1));
+        assertEquals("Extracted Year should be 2020", Integer.valueOf(4), result.get(2));
+        assertEquals("Extracted Year should be 2022", Integer.valueOf(2), result.get(3));
+
     }
 
     /**
