fe166292a5bf77372b6f55456519d7556197c276
Aiswarya R <aiswarya.r6@ibm.com>
2025-06-08 21:42:06 +0530

    Added FAT to test extract() with YEAR field from java.time.LocalDateTime

--- dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartapersistence/src/io/openliberty/jpa/persistence/tests/web/JakartaPersistenceServlet.java
+++ dev/com.ibm.ws.jpa.tests.jpa_32_fat/test-applications/jakartapersistence/src/io/openliberty/jpa/persistence/tests/web/JakartaPersistenceServlet.java
@@ -297,6 +297,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract the calendar YEAR from java.time.LocalDate
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractYearFromLocalData() throws Exception {
@@ -332,6 +334,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract the QUARTER of the year numbered from 1 to 4 from java.time.LocalDate
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractQuarterFromLocalData() throws Exception {
@@ -367,6 +371,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract the MONTH of the year numbered from 1 from java.time.LocalDate
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractMonthFromLocalData() throws Exception {
@@ -402,6 +408,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract the DAY of the year numbered from 1 from java.time.LocalDate
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractDayFromLocalData() throws Exception {
@@ -437,6 +445,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract ISO-8601 week number java.time.LocalDate
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractWeekFromLocalData() throws Exception {
@@ -472,6 +482,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract the The hour of the day in 24-hour time, numbered from 0 to 23 from java.time.LocalTime
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractHourFromLocalTime() throws Exception {
@@ -509,6 +521,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract The minute of the hour, numbered from 0 to 59 from java.time.LocalTime
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractMinuteFromLocalTime() throws Exception {
@@ -543,6 +557,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract The second of the minute, numbered from 0 to 59, including a fractional part representing fractions of a second java.time.LocalTime
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractSecondFromLocalTime() throws Exception {
@@ -578,6 +594,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract the calendar YEAR from java.time.LocalDateTime
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractYearFromLocalDateTime() throws Exception {
@@ -613,6 +631,8 @@ public class JakartaPersistenceServlet extends FATServlet {
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
      * this test extract the QUARTER of the year numbered from 1 to 4 from java.time.LocalDateTime
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractQuarterFromLocalDateTime() throws Exception {
@@ -647,7 +667,9 @@ public class JakartaPersistenceServlet extends FATServlet {
 
     /**
      * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
-     * this test extract the MONTH of the year numbered from 1 from java.time.LocalDate
+     * this test extract the MONTH of the year numbered from 1 from java.time.LocalDateTime
+     *
+     * @throws Exception
      */
     @Test
     public void testExtractMonthFromLocalDateTime() throws Exception {
@@ -677,6 +699,43 @@ public class JakartaPersistenceServlet extends FATServlet {
         assertEquals("Extracted Month should be 1", Integer.valueOf(1), result.get(1));
         assertEquals("Extracted Month should be 12", Integer.valueOf(12), result.get(2));
         assertEquals("Extracted Month should be 6", Integer.valueOf(6), result.get(3));
+
+    }
+
+    /**
+     * Jakarta Persistence 3.2 adds extract() to CriteriaBuilder
+     * this test extract the DAY of the year from java.time.LocalDateTime
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testExtractDayFromLocalDateTime() throws Exception {
+        deleteAllEntities(QueryDateTimeEntity.class);
+        QueryDateTimeEntity q1 = new QueryDateTimeEntity(1, "q1", LocalDate.of(2022, 06, 07), LocalTime.of(12, 0), LocalDateTime.of(2022, 06, 07, 12, 0));
+        QueryDateTimeEntity q2 = new QueryDateTimeEntity(2, "q2", LocalDate.of(2020, 12, 31), LocalTime.of(01, 59), LocalDateTime.of(2020, 12, 31, 01, 59));
+        QueryDateTimeEntity q3 = new QueryDateTimeEntity(3, "q3", LocalDate.of(2021, 01, 01), LocalTime.of(00, 30), LocalDateTime.of(2021, 01, 01, 00, 30));
+        QueryDateTimeEntity q4 = new QueryDateTimeEntity(10000);
+
+        tx.begin();
+        em.persist(q1);
+        em.persist(q2);
+        em.persist(q3);
+        em.persist(q4);
+        tx.commit();
+
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Integer> criteriaQuery = criteriaBuilder.createQuery(Integer.class);
+        Root<QueryDateTimeEntity> from = criteriaQuery.from(QueryDateTimeEntity.class);
+        jakarta.persistence.criteria.LocalDateTimeField<Integer> dayLocalDateField = jakarta.persistence.criteria.LocalDateTimeField.DAY;
+        jakarta.persistence.criteria.Expression<Integer> dayExpression = criteriaBuilder.extract(dayLocalDateField, from.get("localDateTimeData"));
+        criteriaQuery.select(dayExpression);
+        criteriaQuery.orderBy(criteriaBuilder.desc(from.get("name"), Nulls.FIRST));
+        List<Integer> result = em.createQuery(criteriaQuery).getResultList();
+        assertEquals(4, result.size());
+        assertEquals(null, result.get(0));
+        assertEquals("Extracted day should be 1", Integer.valueOf(1), result.get(1));
+        assertEquals("Extracted day should be 31", Integer.valueOf(31), result.get(2));
+        assertEquals("Extracted day should be 7", Integer.valueOf(7), result.get(3));
 
     }
 
